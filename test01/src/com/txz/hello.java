21d1f17e21c501544b2f48ada675ced881a6fa4a
LOOOOOOOOOOOOSER <1969722589@qq.com>
2025-07-24 14:13:23 +0800

    111

--- test01/src/com/txz/hello.java
+++ test01/src/com/txz/hello.java
@@ -7,5 +7,6 @@ package com.txz;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world3");
     }
 }
