c520fc2cb14090c80a954d8c7447e4209ab832d0
LOOOOOOOOOOOOSER <1969722589@qq.com>
2025-07-24 13:22:55 +0800

    first

---
+++ test01/src/com/txz/hello.java
@@ -0,0 +1,11 @@
+package com.txz;
+
+/**
+ * @author 谭轩钊
+ * version 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
